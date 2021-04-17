6b4ae9d6a2c8464477cb3522eb1193fc39093293
yubi00 <ubrajkhadka@gmail.com>
2021-04-17 20:18:42 +1000

    deployment setup

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 //Routes
 app.use(router);
 
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
