37e0ecaf040ec75ff666fa9f4585d4d97bd2b789
yubi <ubrajkhadka@gmail.com>
2021-06-08 02:52:59 +1000

    docker setup

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 //serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
