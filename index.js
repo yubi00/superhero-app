56cadee1e2183a6dbf46af3d45ecba28761cd454
yubi00 <ubrajkhadka@gmail.com>
2023-01-01 17:10:18 +1100

    add contentsecuritypolicy

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
 
-const router = require("./routes/superheroes");
+const router = require('./routes/superheroes');
+const { default: helmet } = require('helmet');
 
 const app = express();
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      'default-src': 'none',
+      'img-src': 'self',
+    },
+  })
+);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,9 +23,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 //serve static assets if in production
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
